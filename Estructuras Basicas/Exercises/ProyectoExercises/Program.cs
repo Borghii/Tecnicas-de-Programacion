e3c483798ce7e1b6b5155d28aafafb8da56cd3f7
Tomas Borghi <137845283+Borghii@users.noreply.github.com>
2024-08-16 23:03:09 +0000

    add: ExerciseTwo

---
+++ Estructuras Basicas/Exercises/ProyectoExercises/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ExerciseTwo();
+        
+    }
+
+    public static void ExerciseTwo(){
+
+        long numero;
+        bool esNumeroValido;
+        
+        do
+        {
+            Console.Write("Ingrese un número entero positivo: ");
+            esNumeroValido = long.TryParse(Console.ReadLine() ,out numero);
+
+        } while (!esNumeroValido || numero <= 0);
+
+        String result = numero.ToString();
+
+        Console.Write("Tiene "+result.Length+" caracteres");
+
+
+    }
+        
+    
+}
